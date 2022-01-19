1a2097266ecf5d74ff604d3a475b1d5f351cb600
cc8carat <chloe26.ni@gmail.com>
2022-01-19 17:18:05 +0100

    complete

--- controllers/rooms.js
+++ controllers/rooms.js
@@ -8,7 +8,7 @@ export const getRooms = asyncHandler(async (req, res) => {
   } = req;
 
   if (!longitude | !latitude) throw new ErrorResponse('longitude and latitude are required');
-  const rooms = await Room.find({ location: { $near: { $geometry: { type: 'Point', coordinates: [longitude, latitude] }, $maxDistance: 3500 } } });
+  const rooms = await Room.find({ location: { $near: { $geometry: { type: 'Point', coordinates: [longitude, latitude] }, $maxDistance: 2300 } } });
   res.json(rooms);
 });
 
@@ -28,17 +28,17 @@ export const createRoom = asyncHandler(async (req, res) => {
   res.json(data);
 });
 
-export const checkinRoom = asyncHandler(async (req, res, next) => {
-  const {
-    user: { _id },
-    params: { id: roomId },
-  } = req;
-  const roomFound = await Room.findById(roomId);
-  if (!roomFound) throw new ErrorResponse(`Room with id ${id} does not exsit`, 404);
+// export const checkinRoom = asyncHandler(async (req, res, next) => {
+//   const {
+//     user: { _id },
+//     params: { id: roomId },
+//   } = req;
+//   const roomFound = await Room.findById(roomId);
+//   if (!roomFound) throw new ErrorResponse(`Room with id ${id} does not exsit`, 404);
 
-  //check if user is included in members, if not, push user to members array
-  //   const memberFond = await Room.findById(roomId).find({members: _id});
+//   check if user is included in members, if not, push user to members array
+//     const memberFond = await Room.findById(roomId).find({members: _id});
 
-  const room = await Room.findOneAndUpdate({ _id: roomId }, { $push: { members: _id } }, { new: true }).populate('members');
-  res.json(room);
-});
+//   const room = await Room.findOneAndUpdate({ _id: roomId }, { $push: { members: _id } }, { new: true }).populate('members');
+//   res.json(room);
+// });
